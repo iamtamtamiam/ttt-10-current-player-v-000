61e52be04cb3694ba86d457a0d8f73e8d41bad01
Tam Vu <vutam_a@hotmail.com>
2018-09-17 01:03:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -18,6 +18,14 @@ end
  # end
 #end
 
+#def current_player(board)
+#  if turn_count(board).even?
+ #   return "X"
+  #else
+   # return "O"
+  #end
+#end
+
 def current_player(board)
   if turn_count(board).even?
     return "X"
