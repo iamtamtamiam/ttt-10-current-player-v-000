dfdfd0b1453a912e5c8eac4fdf7300d5d483d21c
Tam Vu <vutam_a@hotmail.com>
2018-09-17 01:01:28 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,8 +10,16 @@ def turn_count(board)
 end
 
 
+#def current_player(board)
+ # if turn_count(board).to_i % 2 == 0
+ #   return "X"
+ # else
+ #   return "O"
+ # end
+#end
+
 def current_player(board)
-  if turn_count(board).to_i % 2 == 0
+  if turn_count(board) % 2 == 0
     return "X"
   else
     return "O"
